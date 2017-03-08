3c218975946245f52e20fe0d6aba2071a3fc62a7
Bedig Galladian <bgalladian@gmail.com>
2017-03-08 18:10:03 -0500

    got index to work

--- js/app.js
+++ js/app.js
@@ -1,34 +1,44 @@
-let postsData = [
-
-  {
-    author: "Logan", body:"Cables the son of original X-Man Scott Summers and the clone of Jean Grey who grew up in a war-torn future ruled by Apocalypse. Like a sci fi Lone Wolf & Cub, the new Cable series is packed with action, adventure, humor and everything else an X-Men fan could ask for. This is where youll find all the big-time action, major storylines and iconic Spider-Man magic youd come to expect from the Wall-Crawler. Iron Man, Thor, Captain America and the rest of Earths Mightiest Heroes unite to stand against the threats none can face alone.Amazing Spider-Man is the cornerstone of the Marvel Universe. Youve found it. Like a sci fi Lone Wolf & Cub, the new Cable series is packed with action, adventure, humor and everything else an X-Men fan could ask for. Marvels mighty mutants go worldwide and beyond in this series following Cyclops, Wolverine, Beast, Emma Frost and more in their astonishing adventures.", photoUrl: "http://www.indiewire.com/wp-content/uploads/2017/01/lohan.jpg"
-  },
-  {
-    author: "Laura", body:"Cables the son of original X-Man Scott Summers and the clone of Jean Grey who grew up in a war-torn future ruled by Apocalypse. Like a sci fi Lone Wolf & Cub, the new Cable series is packed with action, adventure, humor and everything else an X-Men fan could ask for. This is where youll find all the big-time action, major storylines and iconic Spider-Man magic youd come to expect from the Wall-Crawler. Iron Man, Thor, Captain America and the rest of Earths Mightiest Heroes unite to stand against the threats none can face alone.Amazing Spider-Man is the cornerstone of the Marvel Universe. Youve found it. Like a sci fi Lone Wolf & Cub, the new Cable series is packed with action, adventure, humor and everything else an X-Men fan could ask for. Marvels mighty mutants go worldwide and beyond in this series following Cyclops, Wolverine, Beast, Emma Frost and more in their astonishing adventures.", photoUrl: "http://nerdist.com/wp-content/uploads/2016/10/Logan-1.jpg"
-  },
-  {
-    author: "Charles", body:"Cables the son of original X-Man Scott Summers and the clone of Jean Grey who grew up in a war-torn future ruled by Apocalypse. Like a sci fi Lone Wolf & Cub, the new Cable series is packed with action, adventure, humor and everything else an X-Men fan could ask for. This is where youll find all the big-time action, major storylines and iconic Spider-Man magic youd come to expect from the Wall-Crawler. Iron Man, Thor, Captain America and the rest of Earths Mightiest Heroes unite to stand against the threats none can face alone.Amazing Spider-Man is the cornerstone of the Marvel Universe. Youve found it. Like a sci fi Lone Wolf & Cub, the new Cable series is packed with action, adventure, humor and everything else an X-Men fan could ask for. Marvels mighty mutants go worldwide and beyond in this series following Cyclops, Wolverine, Beast, Emma Frost and more in their astonishing adventures.", photoUrl: "http://cdn.collider.com/wp-content/uploads/2016/10/logan-patrick-stewart.jpg"
-  }
-]
-
 angular
-  .module("wdinstagram", ["ui.router"])
+  .module("wdinstagram", [
+    "ui.router",
+    "ngResource"
+  ])
+
   .config([
    "$stateProvider",
     RouterFunction
   ])
-.controller("PostsController",  ["$stateParams", PostsControllerFunction])
 
-  function PostsControllerFunction($state, $stateParams) {
-    this.posts = postsData;
+  .factory("PostsFactory", [
+    "$resource",
+    PostsFactoryFunction
+  ])
+
+  .controller("PostsIndexController", [
+    "PostsFactory",
+    PostsIndexControllerFunction
+  ])
+
+
+  function PostsFactoryFunction( $resource ) {
+    return $resource( "http://localhost:3000/entries/:id");
   }
+
+  function PostsIndexControllerFunction( PostsFactory ){
+    this.posts = PostsFactory.query();
+  }
+
 
   function RouterFunction($stateProvider){
     $stateProvider
     .state("postsIndex", {
-      url: "/",
-      templateUrl: "posts-index.html",
-      controller: "PostsController",
+      url: "/posts",
+      templateUrl: "js/ng-views/index.html",
+      controller: "PostsIndexController",
       controllerAs: "vm"
+    })
+    .state("postsShow", {
+      url: "/posts/:id",
+      templateUrl: "js/ng-views/show.html"
     })
   }
