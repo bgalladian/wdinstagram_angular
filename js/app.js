6b2a4d3281d30b6094d823e6ad0bbd178c652d9b
Bedig Galladian <bgalladian@gmail.com>
2017-03-08 19:20:17 -0500

    full CRUD

--- js/app.js
+++ js/app.js
@@ -19,14 +19,24 @@ angular
     PostsIndexControllerFunction
   ])
 
+  .controller("PostShowController", [
+    "PostsFactory",
+    "$stateParams",
+    PostsShowControllerFunction
+  ])
 
-  function PostsFactoryFunction( $resource ) {
-    return $resource( "http://localhost:3000/entries/:id");
-  }
+  .controller("PostNewController", [
+    "PostsFactory",
+    "$state",
+    PostNewControllerFunction
+  ])
 
-  function PostsIndexControllerFunction( PostsFactory ){
-    this.posts = PostsFactory.query();
-  }
+  .controller("PostEditController", [
+    "PostsFactory",
+    "$stateParams",
+    "$state",
+    PostEditControllerFunction
+  ])
 
 
   function RouterFunction($stateProvider){
@@ -37,8 +47,60 @@ angular
       controller: "PostsIndexController",
       controllerAs: "vm"
     })
-    .state("postsShow", {
-      url: "/posts/:id",
-      templateUrl: "js/ng-views/show.html"
+    .state("postNew", {
+      url: "/posts/new",
+      templateUrl: "js/ng-views/new.html",
+      controller: "PostNewController",
+      controllerAs: "vm"
     })
+    .state("postShow", {
+      url: "/posts/:id",
+      templateUrl: "js/ng-views/show.html",
+      controller: "PostShowController",
+      controllerAs: "vm"
+    })
+    .state("postEdit", {
+      url: "/posts/:id/edit",
+      templateUrl: "js/ng-views/edit.html",
+      controller: "PostEditController",
+      controllerAs: "vm"
+    })
+  }
+
+
+  function PostsFactoryFunction( $resource ) {
+    return $resource( "http://localhost:3000/entries/:id", {}, {
+      update: {method: "PUT" }
+    });
+  }
+
+  function PostsIndexControllerFunction( PostsFactory ){
+    this.posts = PostsFactory.query();
+  }
+
+  function PostNewControllerFunction( PostsFactory, $state ){
+    this.post = new PostsFactory();
+    this.create = function(){
+      this.post.$save(function(post) {
+        $state.go("postShow", {id: post.id})
+      })
+    }
+  }
+
+  function PostsShowControllerFunction(PostsFactory, $stateParams) {
+    this.post = PostsFactory.get({id: $stateParams.id});
+  }
+
+  function PostEditControllerFunction (PostFactory, $stateParams, $state) {
+    this.post = PostFactory.get({id: $stateParams.id});
+    this.update = function() {
+      this.post.$update({id: $stateParams.id}, function (post){
+        $state.go("postShow", {id: post.id})
+      })
+    }
+    this.destroy = function(){
+      this.post.$delete({id: $stateParams.id}, function (post){
+        $state.go('postsIndex')
+      });
+    }
   }
